d84a30f640556ec5dea5d9ceca3606538da6f4c7
John Lees-Miller <jdleesmiller@gmail.com>
2012-06-01 23:32:22 +0100

    doc cleanups

--- monitor/monitor.R
+++ monitor/monitor.R
@@ -89,32 +89,3 @@ monitor.jd <- function(lastTimeLong = -1, max.records=500) {
   }
 }
 
-library(fields)
-
-time.start <- as.numeric(as.POSIXct('2012-05-31 12:01:38'))*1000
-time.end <-   as.numeric(as.POSIXct('2012-05-31 12:16:58'))*1000
-
-#time.start <- as.numeric(as.POSIXct('2012-05-31 12:39:00'))*1000
-#time.end <-   as.numeric(as.POSIXct('2012-05-31 12:54:00'))*1000
-
-gps.bind.speed <- function(gps) {
-  do.call(rbind, by(gps, gsp$device_id, function(gps.d) {
-    gps.d <- gps.d[order(gps.d$time),]
-    gps.d$step.dist <- NA
-    gps.d$step.time <- NA
-    n <- nrow(gps.d)
-    for (i in 2:n) {
-      pos.1 <- with(gps.d[i-1,], matrix(c(longitude,latitude), 1, 2))
-      pos.2 <- with(gps.d[i,], matrix(c(longitude,latitude), 1, 2))
-      gps.d$step.dist[i] <- rdist.earth(pos.1, pos.2, miles=FALSE)*1000
-      gps.d$step.time[i] <- gps.d$time[i] - gps.d$time[i-1]
-    }
-    gps.d$speed <- with(gps.d, step.dist / step.time)
-    gps.d
-  }))
-}
-
-cn <- odbcConnect('track_jd')
-gps <- sqlQuery(cn, 'SELECT * FROM gps_records')
-gps.bind.speed(gps)
-
