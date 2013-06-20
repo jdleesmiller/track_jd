722689bac34dfd75e79dfad620448356f7994060
John Lees-Miller <jdleesmiller@gmail.com>
2013-06-20 15:03:05 +0100

    Added configurable delay between Bluetooth scans.

--- TrackJD/src/org/thereflectproject/trackjd/collector/BluetoothCollector.java
+++ TrackJD/src/org/thereflectproject/trackjd/collector/BluetoothCollector.java
@@ -1,5 +1,6 @@
 package org.thereflectproject.trackjd.collector;
 
+import org.thereflectproject.trackjd.Constants;
 import org.thereflectproject.trackjd.TrackJDService;
 import org.thereflectproject.trackjd.data.BluetoothPoint;
 
@@ -9,24 +10,30 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Handler;
 
 /**
  * Repeatedly scan for Bluetooth devices.
  */
 public class BluetoothCollector extends AbstractCollector {
-  
-  private boolean registered;
+
   private BluetoothAdapter bluetoothAdapter;
   private BroadcastReceiver bluetoothReceiver;
   private BroadcastReceiver bluetoothDiscoveryFinishedReceiver;
 
+  private final Handler handler;
+
+  /**
+   * In milliseconds.
+   */
+  private static final int DEFAULT_INTERVAL = 10000;
+
   public BluetoothCollector(TrackJDService context) {
     super(context);
-    
-    registered = false;
 
     bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
+    // this is called when we have a detection
     bluetoothReceiver = new BroadcastReceiver() {
       @Override
       public void onReceive(Context context, Intent intent) {
@@ -36,35 +43,54 @@ public class BluetoothCollector extends AbstractCollector {
       }
     };
 
-    // when a scan finishes, start a new one
+    // this is called when a scan finishes
     bluetoothDiscoveryFinishedReceiver = new BroadcastReceiver() {
       @Override
       public void onReceive(Context context, Intent intent) {
         if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(intent
             .getAction())) {
-          bluetoothAdapter.startDiscovery();
+          logPoint(BluetoothPoint.newEndScanRecord());
+          startScanAfterDelay();
         }
       }
     };
+
+    // used to insert a configurable delay between scans
+    handler = new Handler();
   }
 
   @Override
   public void start() {
-    if (bluetoothAdapter.isEnabled()) {
-      registered = true;
-      getContext().registerReceiver(bluetoothReceiver,
-          new IntentFilter(BluetoothDevice.ACTION_FOUND));
-      getContext().registerReceiver(bluetoothDiscoveryFinishedReceiver,
-          new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
-      bluetoothAdapter.startDiscovery();
-    }
+    getContext().registerReceiver(bluetoothReceiver,
+        new IntentFilter(BluetoothDevice.ACTION_FOUND));
+    getContext().registerReceiver(bluetoothDiscoveryFinishedReceiver,
+        new IntentFilter(BluetoothAdapter.ACTION_DISCOVERY_FINISHED));
+    startScanAfterDelay();
   };
 
   @Override
   public void stop() {
-    if (registered) {
-      getContext().unregisterReceiver(bluetoothReceiver);
-      getContext().unregisterReceiver(bluetoothDiscoveryFinishedReceiver);
-    }
+    getContext().unregisterReceiver(bluetoothReceiver);
+    getContext().unregisterReceiver(bluetoothDiscoveryFinishedReceiver);
   };
+
+  /**
+   * Start a new scan after the interval has elapsed. Note that if Bluetooth
+   * is disabled, this method calls itself to start a new scan.
+   */
+  private void startScanAfterDelay() {
+    handler.postDelayed(
+        new Runnable() {
+          public void run() {
+            if (bluetoothAdapter.isEnabled()) {
+              bluetoothAdapter.startDiscovery();
+              logPoint(BluetoothPoint.newStartScanRecord());
+            } else {
+              startScanAfterDelay();
+            }
+          }
+        },
+        getPreferences().getInt(Constants.PREF_BLUETOOTH_INTERVAL,
+            DEFAULT_INTERVAL));
+  }
 }
--- TrackJD/src/org/thereflectproject/trackjd/data/BluetoothPoint.java
+++ TrackJD/src/org/thereflectproject/trackjd/data/BluetoothPoint.java
@@ -6,14 +6,50 @@ import android.content.Intent;
 /**
  * A single Bluetooth device detection. We record the detected device's
  * Bluetooth MAC address, and also the Received Signal Strength Indicator
- * (RSSI) for the detection.
+ * (RSSI) for the detection. We also abuse this class to record two special
+ * bdaddrs for the start and end of the scan.
  */
 public class BluetoothPoint extends AbstractPoint {
-
   private final String bdaddr;
   private final short rssi;
+  
+  private static final String START_SCAN_BDADDR = "_SCAN_START";
+  
+  private static final String END_SCAN_BDADDR = "_SCAN_END";
+  
+  /**
+   * A dummy record that marks the start of a scan.
+   * 
+   * @return not null
+   */
+  public static BluetoothPoint newStartScanRecord() {
+    return new BluetoothPoint(START_SCAN_BDADDR, (short)0);
+  }
+  
+  /**
+   * A dummy record that marks the end of a scan.
+   * 
+   * @return not null
+   */
+  public static BluetoothPoint newEndScanRecord() {
+    return new BluetoothPoint(END_SCAN_BDADDR, (short)0);
+  }
+  
+  /**
+   * Constructor.
+   * 
+   * @param bdaddr
+   * @param rssi
+   */
+  public BluetoothPoint(String bdaddr, short rssi) {
+    super(System.currentTimeMillis());
+    this.bdaddr = bdaddr;
+    this.rssi = rssi;
+  }
 
   /**
+   * Construct from the intent that we get when we have a detection.
+   * 
    * @param intent
    *          a BluetoothDevice.ACTION_FOUND intent
    */
